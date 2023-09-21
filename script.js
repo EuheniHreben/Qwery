7d7f4282c4ee53d73cf33f802626759237ca79db
EuheniHreben <tomasblack3@gmail.com>
2023-09-21 19:09:55 +0300

    fixies 21.09.23

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const scrollButton = document.getElementById('scrollButton');
 const wrapper = document.querySelector('.wrapper')
+
 document.body.onscroll = function (e) {
   wrapper.style.setProperty('--posY', `${window.scrollY - window.scrollY/2}px`)
 }
