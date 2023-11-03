1449c4dc1caf377c06b91126b4de7fd615cc9835
EuheniHreben <tomasblack3@gmail.com>
2023-11-03 19:30:52 +0300

    fixies 03.11.23

--- js/swiper.js
+++ js/swiper.js
@@ -5,19 +5,12 @@ const swiper = new Swiper('.swiper', {
   autoplay: {
     delay: 5000,
   },
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // effect: 'flip',
+  // effect: 'fade',
+  effect: 'cube',
 });
