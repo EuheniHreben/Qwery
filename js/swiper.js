67fc81ab6a832bc0e50636ddf01441864bc7fa46
EuheniHreben <tomasblack3@gmail.com>
2025-06-09 20:27:13 +0300

    09.06.25

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,12 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
   autoplay: {
-    delay: 2000,
+    delay: 4000,
   },
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  // effect: 'flip',
-  // effect: 'fade',
-  // effect: 'cube',
+  effect: 'cube'
 });
